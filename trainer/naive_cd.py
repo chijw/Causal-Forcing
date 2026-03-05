b534eb40e1acebeb17b35a87025ef65415a0629f
Hongzhou Zhu <138758655+zhuhz22@users.noreply.github.com>
2026-03-05 13:29:04 +0800

    Update naive_cd.py, fix frame-wise bug

--- trainer/naive_cd.py
+++ trainer/naive_cd.py
@@ -1,306 +1,181 @@
-import gc
-import logging
-from utils.dataset import cycle
-from utils.dataset import LatentLMDBDataset
-from utils.distributed import EMA_FSDP, fsdp_wrap, fsdp_state_dict, launch_distributed_job
-from utils.misc import (
-    set_seed,
-    merge_dict_list
-)
-import torch.distributed as dist
-from omegaconf import OmegaConf
+import torch.nn.functional as F
+from typing import Tuple
 import torch
-import wandb
-import time
-import os
-from model import NaiveConsistency
-
-
-
-class Trainer:
-    def __init__(self, config):
-        self.config = config
-        self.step = 0
-
-        # Step 1: Initialize the distributed training environment (rank, seed, dtype, logging etc.)
-        torch.backends.cuda.matmul.allow_tf32 = True
-        torch.backends.cudnn.allow_tf32 = True
-
-        launch_distributed_job()
-        global_rank = dist.get_rank()
-        self.world_size = dist.get_world_size()
-
-        self.dtype = torch.bfloat16 if config.mixed_precision else torch.float32
-        self.device = torch.cuda.current_device()
-        self.is_main_process = global_rank == 0
-        self.causal = config.causal
-        self.disable_wandb = config.disable_wandb
-
-        # use a random seed for the training
-        if config.seed == 0:
-            random_seed = torch.randint(0, 10000000, (1,), device=self.device)
-            dist.broadcast(random_seed, src=0)
-            config.seed = random_seed.item()
-
-        set_seed(config.seed + global_rank)
-
-        if self.is_main_process and not self.disable_wandb:
-            wandb.login(host=config.wandb_host, key=config.wandb_key)
-            wandb.init(
-                config=OmegaConf.to_container(config, resolve=True),
-                name=config.config_name,
-                mode="online",
-                entity=config.wandb_entity,
-                project=config.wandb_project,
-                dir=config.wandb_save_dir
-            )
-
-        self.output_path = config.logdir
-
-        # Step 2: Initialize the model and optimizer
-        self.model = NaiveConsistency(config, device=self.device)
-
-        self.model.generator = fsdp_wrap(
-            self.model.generator,
-            sharding_strategy=config.sharding_strategy,
-            mixed_precision=config.mixed_precision,
-            wrap_strategy=config.generator_fsdp_wrap_strategy,
-            cpu_offload=True
-        )
+import random
+from model.base import BaseModel
+from utils.wan_wrapper import WanDiffusionWrapper, WanTextEncoder, WanVAEWrapper
+from utils.scheduler import FlowMatchScheduler
+from pipeline import CausalDiffusionInferencePipeline
+class NaiveConsistency(BaseModel):
+    def __init__(self, args, device):
+        super().__init__(args, device)
+        print(args)
+        # Step 1: Initialize all models
+        self.generator = WanDiffusionWrapper(**getattr(args, "model_kwargs", {}), is_causal=args.is_causal)
+        self.generator.model.requires_grad_(True)
         
-        self.model.generator_ema = fsdp_wrap(
-            self.model.generator_ema,
-            sharding_strategy=config.sharding_strategy,
-            mixed_precision=config.mixed_precision,
-            wrap_strategy=config.generator_fsdp_wrap_strategy,
-            cpu_offload=True
-        )
-
-        self.model.teacher = fsdp_wrap(
-            self.model.teacher,
-            sharding_strategy=config.sharding_strategy,
-            mixed_precision=config.mixed_precision,
-            wrap_strategy=config.real_score_fsdp_wrap_strategy,
-            cpu_offload=True
-        )
-
-        self.model.text_encoder = fsdp_wrap(
-            self.model.text_encoder,
-            sharding_strategy=config.sharding_strategy,
-            mixed_precision=config.mixed_precision,
-            wrap_strategy=config.text_encoder_fsdp_wrap_strategy,
-            cpu_offload=True
-        )
-
         
-        self.generator_optimizer = torch.optim.AdamW(
-            [param for param in self.model.generator.parameters()
-             if param.requires_grad],
-            lr=config.lr,
-            betas=(config.beta1, config.beta2),
-            weight_decay=config.weight_decay
-        )
-
-        self.generator_ema = EMA_FSDP(self.model.generator, decay=self.config.ema_weight)
+        self.generator_ema = WanDiffusionWrapper(**getattr(args, "model_kwargs", {}), is_causal=args.is_causal)
+        self.generator_ema.model.requires_grad_(False)
         
-        # Step 3: Initialize the dataloader
- 
-        dataset = LatentLMDBDataset(
-            config.data_path, max_pair=int(1e8))
+        self.teacher = WanDiffusionWrapper(**getattr(args, "model_kwargs", {}), is_causal=True)
+        self.teacher.model.requires_grad_(False)
         
-        sampler = torch.utils.data.distributed.DistributedSampler(
-            dataset, shuffle=True, drop_last=True)
-        dataloader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=config.batch_size,
-            sampler=sampler,
-            num_workers=8)
-
-        if dist.get_rank() == 0:
-            print("DATASET SIZE %d" % len(dataset))
-        self.dataloader = cycle(dataloader)
-
-        
-        ##############################################################################################################
-        # 6. Set up EMA parameter containers
-        rename_param = (
-            lambda name: name.replace("_fsdp_wrapped_module.", "")
-            .replace("_checkpoint_wrapped_module.", "")
-            .replace("_orig_mod.", "")
-        )
-        self.name_to_trainable_params = {}
-        for n, p in self.model.generator.named_parameters():
-            if not p.requires_grad:
-                continue
-
-            renamed_n = rename_param(n)
-            self.name_to_trainable_params[renamed_n] = p
-        ema_weight = config.ema_weight
-        self.generator_ema = None
-        if (ema_weight is not None) and (ema_weight > 0.0):
-            print(f"Setting up EMA with weight {ema_weight}")
-            self.generator_ema = EMA_FSDP(self.model.generator, decay=ema_weight)
-
-        ##############################################################################################################
-        # 7. Load the causal diffusion model as the teacher model
-        if getattr(config, "generator_ckpt", False):
-            print(f"Loading pretrained generator from {config.generator_ckpt}")
-            state_dict = torch.load(config.generator_ckpt, map_location="cpu")
-            if "generator" in state_dict:
-                state_dict = state_dict["generator"]
-                fixed = {}
-                for k, v in state_dict.items():
-                    if k.startswith("model._fsdp_wrapped_module."):
-                        k = k.replace("model._fsdp_wrapped_module.", "model.", 1)
-                    fixed[k] = v
-                state_dict = fixed
-            elif "model" in state_dict:
-                state_dict = state_dict["model"]
-            elif "generator_ema" in state_dict:
-                gen_sd = state_dict["generator_ema"]
-                fixed = {}
-                for k, v in gen_sd.items():
-                    if k.startswith("model._fsdp_wrapped_module."):
-                        k = k.replace("model._fsdp_wrapped_module.", "model.", 1)
-                    fixed[k] = v
-                state_dict = fixed
-                
-                
-            self.model.generator.load_state_dict(
+        self.num_frame_per_block = getattr(args, "num_frame_per_block", 1)
+        if self.num_frame_per_block > 1:
+            self.generator.model.num_frame_per_block = self.num_frame_per_block
+            self.generator_ema.model.num_frame_per_block = self.num_frame_per_block
+            self.teacher.model.num_frame_per_block = self.num_frame_per_block
+            
+            
+        if getattr(args, "generator_ckpt", False):
+            print(f"Loading pretrained generator from {args.generator_ckpt}")
+            state_dict = torch.load(args.generator_ckpt, map_location="cpu")[
+                'generator']
+            self.generator.load_state_dict(
                 state_dict, strict=True
             )
-            self.model.teacher.load_state_dict(
+            
+            self.teacher.load_state_dict(
                 state_dict, strict=True
             )
+            
+            self.generator_ema.load_state_dict(
+                state_dict, strict=True
+            )
+                         
+        self.independent_first_frame = getattr(args, "independent_first_frame", False)
+        if self.independent_first_frame:
+            self.generator.model.independent_first_frame = True
+        if args.gradient_checkpointing:
+            self.generator.enable_gradient_checkpointing()
 
-        #############################################################################################################
-        self.max_grad_norm_generator = getattr(config, "max_grad_norm_generator", 10.0)
-        self.max_grad_norm_critic = getattr(config, "max_grad_norm_critic", 10.0)
-        self.previous_time = None
+        # Step 2: Initialize all hyperparameters
+        self.timestep_shift = getattr(args, "timestep_shift", 1.0)
+        self.guidance_scale = args.guidance_scale
+        
+        self.discrete_cd_N = getattr(args, "discrete_cd_N", 48)
+        self.scheduler = FlowMatchScheduler(shift=5.0, sigma_min=0.0, extra_one_step=True)
+        self.scheduler.set_timesteps(num_inference_steps=self.discrete_cd_N, denoising_strength=1.0)
+        self.scheduler.sigmas = self.scheduler.sigmas.to(device)
+        
+        self.pipeline = CausalDiffusionInferencePipeline(args, device=device, need_vae=False)
+        self.pipeline.generator = self.teacher
+        self.pipeline.text_encoder = self.text_encoder
+        
+    def _initialize_models(self, args, device):
+        self.generator = WanDiffusionWrapper(**getattr(args, "model_kwargs", {}), is_causal=True)
+        self.generator.model.requires_grad_(True)
+
+        self.teacher = WanDiffusionWrapper(**getattr(args, "model_kwargs", {}), is_causal=True)
+        self.teacher.model.requires_grad_(False)
+
+        self.generator_ema = WanDiffusionWrapper(**getattr(args, "model_kwargs", {}), is_causal=args.is_causal)
+        self.generator_ema.model.requires_grad_(False)
+        
+        self.text_encoder = WanTextEncoder()
+        self.text_encoder.requires_grad_(False)
+
+        
+        self.scheduler = self.generator.get_scheduler()
+        self.scheduler.timesteps = self.scheduler.timesteps.to(device)
+
         
         
-
-    def save(self):
-        print("Start gathering distributed model states...")
-        generator_state_dict = fsdp_state_dict(
-            self.model.generator)
-
-        if self.config.ema_start_step < self.step:
-            state_dict = {
-                "generator_ema": self.generator_ema.state_dict(),
-            }
-        else:
-            state_dict = {
-                "generator": generator_state_dict,
-            }
-
-        if self.is_main_process:
-            os.makedirs(os.path.join(self.output_path,
-                        f"checkpoint_model_{self.step:06d}"), exist_ok=True)
-            torch.save(state_dict, os.path.join(self.output_path,
-                       f"checkpoint_model_{self.step:06d}", "model.pt"))
-            print("Model saved to", os.path.join(self.output_path,
-                  f"checkpoint_model_{self.step:06d}", "model.pt"))
-
-            
-    def fwdbwd_one_step(self, batch, clean_latent=None):
-        self.model.eval()
-
-        if self.step % 20 == 0:
-            torch.cuda.empty_cache()
-
-        # Step 1: Get the next batch of text prompts
-        text_prompts = batch["prompts"]
-        batch_size = len(text_prompts)
-        image_or_video_shape = list(self.config.image_or_video_shape)
-        image_or_video_shape[0] = batch_size
-
-        # Step 2: Extract the conditional infos
-        with torch.no_grad():
-            conditional_dict = self.model.text_encoder(
-                text_prompts=text_prompts)
-
-            if not getattr(self, "unconditional_dict", None):
-                unconditional_dict = self.model.text_encoder(
-                    text_prompts=[self.config.negative_prompt] * batch_size)
-                unconditional_dict = {k: v.detach()
-                                      for k, v in unconditional_dict.items()}
-                self.unconditional_dict = unconditional_dict  # cache the unconditional_dict
-            else:
-                unconditional_dict = self.unconditional_dict
-
-        # Step 3: Store gradients for the generator (if training the generator)
-        generator_loss, generator_log_dict = self.model.generator_loss(
-            conditional_dict=conditional_dict,
-            unconditional_dict=unconditional_dict,
-            clean_latent=clean_latent,
-            ema_model = self.generator_ema
-        )
-        generator_loss.backward()
-        generator_grad_norm = self.model.generator.clip_grad_norm_(
-            self.max_grad_norm_generator)
-
-        generator_log_dict.update({"generator_loss": generator_loss,
-                                    "generator_grad_norm": generator_grad_norm})
-
-        return generator_log_dict
+    def generator_loss(
+            self, 
+            conditional_dict,
+            unconditional_dict,
+            clean_latent,
+            ema_model
+        ) -> Tuple[torch.Tensor, dict]:
         
-
-   
-
-    def train(self):
-        start_step = self.step
-
-        while True:
-
-            self.generator_optimizer.zero_grad(set_to_none=True)
-
-            batch = next(self.dataloader)
-            generator_log_dict = self.fwdbwd_one_step(batch, clean_latent=batch["clean_latent"])
-            
-
-            self.generator_optimizer.step()
-            if self.generator_ema is not None:
-                self.generator_ema.update(self.model.generator)
-            
-              
-
-            # Increment the step since we finished gradient update
-            self.step += 1
-
-           
-            # Save the model
-            if (not self.config.no_save) and (self.step - start_step) > 0 and self.step % self.config.log_iters == 0:
-                torch.cuda.empty_cache()
-                self.save()
-                torch.cuda.empty_cache()
-
-            # Logging
-            if self.is_main_process:
-                wandb_loss_dict = {}
-                wandb_loss_dict.update(
-                        {
-                            "generator_loss": generator_log_dict["generator_loss"].mean().item(),
-                            "generator_grad_norm": generator_log_dict["generator_grad_norm"].mean().item()
-                        }
-                    )
-
-              
-
-                if not self.disable_wandb:
-                    wandb.log(wandb_loss_dict, step=self.step)
-
-            if self.step % self.config.gc_interval == 0:
-                if dist.get_rank() == 0:
-                    logging.info("DistGarbageCollector: Running GC.")
-                gc.collect()
-                torch.cuda.empty_cache()
-
-            if self.is_main_process:
-                current_time = time.time()
-                if self.previous_time is None:
-                    self.previous_time = current_time
+        clean_latent=clean_latent.to(self.device).to(torch.bfloat16)
+        timestep_idx = random.randrange(self.discrete_cd_N-1) 
+        
+        t=self.scheduler.timesteps[timestep_idx]
+        
+        timestep = t * \
+            torch.ones([1, 21], device=self.device, dtype=torch.bfloat16)
+        
+        noise = torch.randn_like(clean_latent).to(self.device)
+        latent_t = self.scheduler.add_noise(clean_latent,noise=noise,timestep=t*torch.ones([1]).to(self.device)).to(torch.bfloat16)
+        
+        
+        pipeline = CausalDiffusionInferencePipeline(self.args, device=self.device,generator=self.teacher,text_encoder=self.text_encoder)
+        latent_t_next = []
+        
+        if self.num_frame_per_block == 3:
+            # chunk-wise, hard-coded now
+            for chunk_idx in range(1,8):
+                if chunk_idx > 1:
+                    initial_latent = clean_latent[:,:3*(chunk_idx-1)]
                 else:
-                    if not self.disable_wandb:
-                        wandb.log({"per iteration time": current_time - self.previous_time}, step=self.step)
-                    self.previous_time = current_time
+                    initial_latent = None
+                
+                latent_t_i = latent_t[:, 3*(chunk_idx-1): 3*chunk_idx]
+                
+                latent_t_next_i = pipeline.inference_for_genuine_cd(
+                    noisy_input=latent_t_i,
+                    conditional_dict=conditional_dict,
+                    unconditional_dict=unconditional_dict,
+                    initial_latent=initial_latent,
+                    timestep_idx=timestep_idx,
+                    sampling_steps=self.discrete_cd_N
+                )
+                latent_t_next.append(latent_t_next_i)
+        else:
+            # frame-wise, hard-coded now
+            for chunk_idx in range(1,22):
+                if chunk_idx > 1:
+                    initial_latent = clean_latent[:,:chunk_idx-1]
+                else:
+                    initial_latent = None
+                
+                latent_t_i = latent_t[:, chunk_idx-1:chunk_idx]
+                
+                latent_t_next_i = pipeline.inference_for_genuine_cd(
+                    noisy_input=latent_t_i,
+                    conditional_dict=conditional_dict,
+                    unconditional_dict=unconditional_dict,
+                    initial_latent=initial_latent,
+                    timestep_idx=timestep_idx,
+                    sampling_steps=self.discrete_cd_N
+                )
+                latent_t_next.append(latent_t_next_i)
+                
+        del pipeline
+        import gc; gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
+        
+        latent_t_next = torch.cat(latent_t_next, dim=1)
+        
+        
+        
+        
+        t_next = self.scheduler.timesteps[timestep_idx + 1]
+        timestep_next = t_next * \
+            torch.ones([1, 21], device=self.device, dtype=torch.bfloat16)
+        print(f't:{t}; t_next: {t_next}')
+        
+        
+        
+        _, cm_pred_t = self.generator(
+            latent_t, conditional_dict, timestep, clean_x = clean_latent
+        )
+
+        with torch.no_grad():
+            ema_model.copy_to(self.generator_ema)
+            _, cm_pred_t_next = self.generator_ema(
+                latent_t_next, conditional_dict, timestep_next, clean_x = clean_latent
+            )
+            
+        with torch.enable_grad():
+            loss = F.mse_loss(cm_pred_t, cm_pred_t_next, reduction="mean")
+
+        log_dict = {
+            "unnormalized_loss": F.mse_loss(cm_pred_t, cm_pred_t_next, reduction='none').mean(dim=[1, 2, 3, 4]).detach(),
+        }
+
+        return loss, log_dict
